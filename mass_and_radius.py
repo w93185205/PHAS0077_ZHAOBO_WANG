7c8b9db70736a0b926fb6a67bea97bfd932974c0
w93185205 <2672117179@qq.com>
2025-08-21 09:43:03 +0100

    Add dictionary of planetary masses and radii
    Created a standalone dictionary containing the mass (kg) and average radius (km) of solar system bodies, including the Sun, planets, and Moon. 
    This file provides a clean and reusable reference for other modules that require physical constants without depending on the ephemeris loader.

---
+++ mass_and_radius.py
@@ -0,0 +1,28 @@
+# mass (kg), radius (km)
+planet_mass = {
+    'sun': 1.9885e30,
+    'mercury': 3.3011e23,
+    'venus': 4.8675e24,
+    'earth': 5.97237e24,
+    'moon': 7.342e22,
+    'mars barycenter': 6.4171e23,
+    'jupiter barycenter': 1.8982e27,
+    'saturn barycenter': 5.6834e26,
+    'uranus barycenter': 8.6810e25,
+    'neptune barycenter': 1.02413e26,
+    'pluto barycenter': 1.303e22
+}
+
+planet_radius = {
+    'sun': 695700,         # average radius, km
+    'mercury': 2439.7,
+    'venus': 6051.8,
+    'earth': 6371.0,
+    'moon': 1737.4,
+    'mars barycenter': 3389.5,
+    'jupiter barycenter': 69911,
+    'saturn barycenter': 58232,
+    'uranus barycenter': 25362,
+    'neptune barycenter': 24622,
+    'pluto barycenter': 1188.3
+}
